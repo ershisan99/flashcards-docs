dc0d9d3c704171ab1be35e24cbf541d9d1fed8a0
Andres <andres99.dev@gmail.com>
2023-06-25 14:10:06 +0200

    fix download link color

--- components/download-link/index.tsx
+++ components/download-link/index.tsx
@@ -1,7 +1,14 @@
 export const DownloadLink = ({ href }: { href: string }) => {
   return (
     <p style={{ marginTop: 24 }}>
-      <a id="raw-url" download href={href} style={{ fontWeight: 700, color: 'blue' }}>
+      <a
+        id="raw-url"
+        download
+        href={href}
+        className={
+          'nx-text-primary-600 nx-underline nx-decoration-from-font [text-underline-position:from-font]'
+        }
+      >
         Скачайте файл watchers.xml
       </a>{' '}
       и импортируйте его в Settings | Tools | File Watchers
